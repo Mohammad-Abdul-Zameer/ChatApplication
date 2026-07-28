19d994f12a1603eca394ca9783155b858c9f2c90
Mohammad-Abdul-Zameer <m.a.zameer6786@gmail.com>
2026-07-29 00:25:05 +0530

    Update ChatServer.java

--- Server/ChatServer.java
+++ Server/ChatServer.java
@@ -1,1 +1,50 @@
+package Server;
 
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+
+    static ArrayList<ClientHandler> clientList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            System.out.println("Chat Server Started...");
+            MessageDAO.showMessages();
+            System.out.println("Waiting for clients...");
+
+            while(true) {
+
+                Socket socket = serverSocket.accept();
+
+                System.out.println("New Client Connected");
+
+                ClientHandler client =
+                        new ClientHandler(socket, clientList);
+
+                clientList.add(client);
+
+                Thread thread = new Thread(client);
+                thread.start();
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void broadcastMessage(String message,
+                                        ClientHandler sender) {
+
+        for(ClientHandler client : clientList) {
+
+            if(client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
+}
