8d53a37794812e318f5ee61a4e75f4fe032d6480
Mohammad-Abdul-Zameer <m.a.zameer6786@gmail.com>
2026-07-29 00:27:02 +0530

    Create DatabaseConnection.java

---
+++ Server/DatabaseConnection.java
@@ -0,0 +1,49 @@
+package Server;
+
+import java.sql.*;
+
+
+public class DatabaseConnection {
+
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/chatapp";
+
+
+    private static final String USER =
+            "root";
+
+
+    private static final String PASSWORD =
+            "your_password";
+
+
+    public static Connection getConnection()
+    {
+
+        Connection con=null;
+
+        try{
+
+            Class.forName(
+              "com.mysql.cj.jdbc.Driver");
+
+
+            con = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD);
+
+
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+
+
+        return con;
+    }
+
+}
