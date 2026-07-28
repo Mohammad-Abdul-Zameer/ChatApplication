9d431474e0acb6f4cc77c96e59292986f3522048
Mohammad-Abdul-Zameer <m.a.zameer6786@gmail.com>
2026-07-29 00:23:33 +0530

    Update ChatClient.java

--- Client/ChatClient.java
+++ Client/ChatClient.java
@@ -1,1 +1,96 @@
+package Client;
 
+
+import java.net.*;
+import java.io.*;
+
+
+public class ChatClient {
+
+
+    Socket socket;
+
+    BufferedReader reader;
+    PrintWriter writer;
+
+
+
+    public ChatClient(String username){
+
+
+        try {
+
+
+            socket = new Socket(
+                    "localhost",
+                    5000);
+
+
+            reader = new BufferedReader(
+                    new InputStreamReader(
+                            socket.getInputStream()));
+
+
+            writer = new PrintWriter(
+                    socket.getOutputStream(),
+                    true);
+
+
+
+            new Thread(new ReceiveMessage())
+                    .start();
+
+
+
+        }
+        catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+
+    public void sendMessage(String message){
+
+        writer.println(message);
+
+    }
+
+
+
+
+    class ReceiveMessage implements Runnable{
+
+
+        public void run(){
+
+            try{
+
+                String message;
+
+
+                while((message =
+                        reader.readLine()) != null){
+
+
+                    ClientGUI.chatArea.append(
+                            message + "\n");
+
+                }
+
+
+            }
+            catch(Exception e){
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+}
